2486b6f1c7d97f3bcd964db87936987bcf00c150
livos <lvivancos2nd-dev@yahoo.fr>
2022-11-29 20:37:51 +0700

    Set empty tests

--- app.test.js
+++ app.test.js
@@ -2,5 +2,9 @@ const request = require("supertest");
 const app = require("./app");
 
 describe("Todos API", () => {
-  it("should run", () => {});
+  it("GET /todos --> array todos", () => {});
+  it("GET /todos/id --> specific toto by ID", () => {});
+  it("GET /todos/id --> 404 if not found", () => {});
+  it("POST /todos --> created todo", () => {});
+  it("GET /todos --> validates request body", () => {});
 });
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 From course https://www.yotube.com/watch?v=M44umyYPiuo
+
+These are supposed to be tests for a todo list app
